dd7ee1430fc84f63eac4a0ee0919ce479396d011
josh <josh@joshs-MacBook-Air.local>
2016-07-14 18:09:09 -0600

    song samples now open in another window by defualt

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -4,14 +4,14 @@ angular.module('itunes').controller('mainCtrl', function($scope, itunesService){
   //this means when you make your iTunes request, you'll need to get back the information, parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
   $scope.gridOptions = {
       data: 'songData',
-      height: '110px',
+      height: '120px',
       sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
       columnDefs: [
-        {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
-        {field: 'Artist', displayName: 'Artist'},
-        {field: 'Collection', displayName: 'Collection'},
+        {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a target="_blank" href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
+		  {field: 'Type', displayName: 'SongTitle'},
+        {field: 'Collection', displayName: 'Album'},
+		  {field: 'Artist', displayName: 'Artist'},
         {field: 'AlbumArt', displayName: 'Album Art', width: '110px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img src="{{row.getProperty(col.field)}}"></div>'},
-        {field: 'Type', displayName: 'SongTitle'},
         {field: 'CollectionPrice', displayName: 'Collection Price'},
       ]
   };
